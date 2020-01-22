18b3928490fc725fd8d081417d82a6fb1c44dfd5
g00387822 <59929561+g00387822@users.noreply.github.com>
2020-01-22 21:45:43 +0000

    Update python_bmi.py

--- python_bmi.py
+++ python_bmi.py
@@ -1,5 +1,10 @@
+#set variables weight and height and collect user input
 weight = float(input("Enter your weight in kilograms: "))
 height = float(input("Enter your height in centimeters: "))
+
+#perform BMI calculation
 conversion_of_height_cm_to_meters = height / 100
 bmi = weight / (conversion_of_height_cm_to_meters ** 2)
+
+#print formatting method may not work in older versions python
 print('BMI is: {:.2f}'.format(bmi))
