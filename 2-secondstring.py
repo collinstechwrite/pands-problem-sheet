e57a84c6cdbf8386eb34f062b8107f8a0425cfa8
g00387822 <59929561+g00387822@users.noreply.github.com>
2020-02-04 12:24:35 +0000

    Add files via upload

---
+++ 2-secondstring.py
@@ -0,0 +1,4 @@
+#code below uses string slicing to reverse print every 2nd letter
+sentence = input("Input a sentence that you want reversed:")
+print("This is your sentence reversed printing from end skipping every second letter")
+print(sentence[::-2])
